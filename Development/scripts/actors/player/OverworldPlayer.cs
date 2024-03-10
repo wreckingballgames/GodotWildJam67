d90cd79a3dc5457ae303a312f2e37106b81efc4c
Raymond McDonald <contact@wreckingballpublishing.com>
2024-03-10 05:58:22 -0500

    Add process method to OverworldPlayer for explicit call of base process method

--- Development/scripts/actors/player/OverworldPlayer.cs
+++ Development/scripts/actors/player/OverworldPlayer.cs
@@ -8,6 +8,11 @@ public partial class OverworldPlayer : OverworldActor
         base._Ready();
     }
 
+    public override void _Process(double _delta)
+    {
+        base._Process(_delta);
+    }
+
     protected override void OnCommandReceived(Command command)
     {
         base.OnCommandReceived(command);
