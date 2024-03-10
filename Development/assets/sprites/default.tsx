2644625b4094631f6f0e25f0038fbb016d348965
Raymond McDonald <contact@wreckingballpublishing.com>
2024-03-10 07:54:46 -0500

    Modify .gitignore to exclude .aseprite files; create a bunch of sprites; create a Tiled project with a tileset and a map

---
+++ Development/assets/sprites/default.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="default" tilewidth="32" tileheight="32" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="fort.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="grass.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="horizontal_fence1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="horizontal_fence2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="horizontal_fence3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="town.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="vertical_fence1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="vertical_fence2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="vertical_fence3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="water1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="water2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="corner_fence1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="corner_fence2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="corner_fence3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="corner_fence4.png"/>
+ </tile>
+</tileset>
