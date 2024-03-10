718f8cb7fb2e961b62fd7d82270b44358bca34dc
Raymond McDonald <contact@wreckingballpublishing.com>
2024-03-10 04:27:33 -0500

    Create Command class for controllers to dispatch commands to actors; lay out some properties and methods for OverworldActorController

---
+++ Development/scripts/actors/Command.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public abstract partial class Command : Node
+{
+}
--- Development/scripts/actors/OverworldActorController.cs
+++ Development/scripts/actors/OverworldActorController.cs
@@ -1,6 +1,38 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public abstract partial class OverworldActorController : Node
 {
+    [Signal]
+    public delegate void CommandDispatchedEventHandler(Command command);
+
+    protected List<Command> Commands {get; set;}
+
+    /// <summary>
+    /// Handle input to determine which commands to dispatch.
+    /// </summary>
+    protected virtual void HandleInput()
+    {
+        // Pseudocode
+        // Read input source for next input
+        // Process input (is it valid? does it cause a return or does it compound?)
+        // Add command to list of commands accordingly
+        // Return
+    }
+
+    /// <summary>
+    /// Emit a signal with the command to dispatch, ideally received by the
+    /// actor this controller is controlling. To be used in DispatchCommandSequence().
+    /// </summary>
+    protected virtual void DispatchCommand()
+    {
+    }
+
+    /// <summary>
+    /// Dispatch any number of commands in sequence.
+    /// </summary>
+    protected virtual void DispatchCommandSequence()
+    {
+    }
 }
