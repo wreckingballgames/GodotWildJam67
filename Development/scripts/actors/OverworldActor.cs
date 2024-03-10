7ef9092149c5fc0f78ac047529578470a77499fc
Raymond McDonald <contact@wreckingballpublishing.com>
2024-03-10 05:57:43 -0500

    Add method to constrain actors to viewport

--- Development/scripts/actors/OverworldActor.cs
+++ Development/scripts/actors/OverworldActor.cs
@@ -11,11 +11,20 @@ public partial class OverworldActor : Node2D
         Controller.CommandDispatched += (Command command) => OnCommandReceived(command);
     }
 
+    public override void _Process(double delta)
+    {
+        EnforceScreenBounds();
+    }
+
     protected virtual void OnCommandReceived(Command command)
     {
         HandleMovement(command);
     }
 
+    /// <summary>
+    /// Use command received from controller to apply movement to actor.
+    /// </summary>
+    /// <param name="command"></param>
     protected void HandleMovement(Command command)
     {
         // TODO: Consider rewriting this class in GDScript as I believe this operation causes marshalling
@@ -42,5 +51,31 @@ public partial class OverworldActor : Node2D
         }
 
         GlobalPosition = newGlobalPosition;
+    }
+
+    /// <summary>
+    /// Constrain actor within screen boundaries.
+    /// </summary>
+    protected void EnforceScreenBounds()
+    {
+        Rect2 viewportRect = GetViewportRect();
+
+        if (GlobalPosition.X < viewportRect.Position.X)
+        {
+            GlobalPosition = GlobalPosition with { X = viewportRect.Position.X };
+        }
+        else if (GlobalPosition.X > viewportRect.Size.X)
+        {
+            GlobalPosition = GlobalPosition with { X = viewportRect.Size.X };
+        }
+
+        if (GlobalPosition.Y < viewportRect.Position.Y)
+        {
+            GlobalPosition = GlobalPosition with { Y = viewportRect.Position.Y };
+        }
+        else if (GlobalPosition.Y > viewportRect.Size.Y)
+        {
+            GlobalPosition = GlobalPosition with { Y = viewportRect.Size.Y };
+        }
     }
 }
