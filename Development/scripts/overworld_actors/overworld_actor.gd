9cbe94b3524566415810032788eaf5deffe1a92f
Raymond McDonald <contact@wreckingballpublishing.com>
2024-03-11 14:21:04 -0500

    Implement forward vector, turning before moving ahead method of movement, and obstacles blocking actors

--- Development/scripts/overworld_actors/overworld_actor.gd
+++ Development/scripts/overworld_actors/overworld_actor.gd
@@ -7,15 +7,20 @@ extends Node2D
 @export var trigger_collider: Area2D
 @export var hazard_collider: Area2D
 
-@onready var last_position: Vector2 = global_position
+var forward: Vector2 = Vector2.RIGHT
+var is_obstacle_ahead: bool = false
 
 
 func _ready() -> void:
+    # Connect signals
     controller.command_dispatched.connect(on_command_received)
+    obstacle_collider.body_entered.connect(_on_obstacle_collider_body_entered)
+    obstacle_collider.body_exited.connect(_on_obstacle_collider_body_exited)
 
 
 func _physics_process(delta: float) -> void:
     enforce_screen_bounds()
+    update_obstacle_collider_position()
 
 
 func on_command_received(command: Command) -> void:
@@ -24,7 +29,14 @@ func on_command_received(command: Command) -> void:
 
 
 func handle_movement(command: MoveCommand) -> void:
-    last_position = global_position
+    # If forward is not the same direction as command direction, turn, don't move
+    # Otherwise, move in that direction (i.e., the direction the actor is already facing)
+    if forward != command.direction:
+        forward = command.direction
+        return
+    
+    if is_obstacle_ahead:
+        return
     
     if command.direction == Vector2.LEFT:
         global_position.x -= GlobalData.TileInfo.TILE_WIDTH
@@ -37,8 +49,19 @@ func handle_movement(command: MoveCommand) -> void:
         global_position.y += GlobalData.TileInfo.TILE_HEIGHT
 
 
-func handle_obstacle_collision(body: Node2D) -> void:
-    global_position = last_position;
+func update_obstacle_collider_position() -> void:
+    if forward == Vector2.LEFT or forward == Vector2.RIGHT:
+        obstacle_collider.global_position = global_position + (forward * GlobalData.ActorInfo.ACTOR_WIDTH)
+    if forward == Vector2.UP or forward == Vector2.DOWN:
+        obstacle_collider.global_position = global_position + (forward * GlobalData.ActorInfo.ACTOR_HEIGHT)
+
+
+func _on_obstacle_collider_body_entered(body: Node2D) -> void:
+    is_obstacle_ahead = true
+
+
+func _on_obstacle_collider_body_exited(body: Node2D) -> void:
+    is_obstacle_ahead = false
 
 
 func handle_trigger_collision(body: Node2D) -> void:
