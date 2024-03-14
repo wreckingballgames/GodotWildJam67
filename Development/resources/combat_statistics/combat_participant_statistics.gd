30d509883d7616529836432128cff7508cee8124
Raymond McDonald <contact@wreckingballpublishing.com>
2024-03-14 10:16:12 -0500

    Set up Resources for combat participant statistics, player combat statistics, and player inventory; add references to player's resources to player persistent statistics

---
+++ Development/resources/combat_statistics/combat_participant_statistics.gd
@@ -0,0 +1,13 @@
+class_name CombatParticipantStatistics
+extends Resource
+
+
+@export var strength: int = 0
+@export var agility: int = 0
+@export var toughness: int = 0
+@export var experience_points: int = 0
+@export var experience_level: int = 0
+@export var max_health: int = 0
+@export var current_health: int = 0
+@export var max_tp: int = 0
+@export var current_tp: int = 0
